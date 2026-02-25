b4632c060ca3e7dd2c427dc8412c52736b6be5d9
madeline <qwertytrogi@gmail.com>
2026-02-24 16:23:47 -0800

    init

---
+++ client.py
@@ -0,0 +1,20 @@
+import asyncio
+from picows import ws_connect, WSFrame, WSTransport, WSListener, WSMsgType, WSCloseCode
+
+class ClientListener(WSListener):
+    def on_ws_connected(self, transport: WSTransport):
+        transport.send(WSMsgType.TEXT, b"Hello world")
+
+    def on_ws_frame(self, transport: WSTransport, frame: WSFrame):
+        print(f"Echo reply: {frame.get_payload_as_ascii_text()}")
+        transport.send_close(WSCloseCode.OK)
+        transport.disconnect()
+
+
+async def main(url):
+    transport, client = await ws_connect(ClientListener, url)
+    await transport.wait_disconnected()
+
+
+if __name__ == '__main__':
+    asyncio.run(main("ws://127.0.0.1:9001"))
---
+++ server.py
@@ -0,0 +1,56 @@
+import asyncio
+from picows import ws_create_server, WSFrame, WSTransport, WSListener, WSMsgType, WSUpgradeRequest
+
+import yaml
+
+with open("multitp_config.yml") as stream:
+    try:
+        CONFIG = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print("Failed to load config:", exc)
+
+
+class Room():
+    admin: "ClientListener"
+    clients: list["ClientListener"]
+    code: str
+
+    def __init__(self, code: str, admin: "ClientListener"):
+        self.code = code
+        self.admin = admin
+        self.add_client(admin)
+    
+    def add_client(self, client: "ClientListener"):
+        self.clients.append(client)
+        client.room = self
+
+class ClientListener(WSListener):
+    room: Room
+
+    def on_ws_connected(self, transport: WSTransport):
+        print(f"New connection from {transport.underlying_transport.get_extra_info('peername')}")
+
+    def on_ws_frame(self, transport: WSTransport, frame: WSFrame):
+        if frame.msg_type == WSMsgType.CLOSE:
+            transport.send_close(frame.get_close_code(), frame.get_close_message())
+            transport.disconnect()
+        else:
+            transport.send(frame.msg_type, frame.get_payload_as_bytes())
+
+async def main():
+    def listener_factory(r: WSUpgradeRequest):
+        return ClientListener()
+
+    server: asyncio.Server = await ws_create_server(
+        listener_factory,
+        CONFIG.get("server").get("ip"),
+        CONFIG.get("server").get("port")
+    )
+    
+    for s in server.sockets:
+        print(f"Server started on {s.getsockname()}")
+
+    await server.serve_forever()
+
+if __name__ == '__main__':
+  asyncio.run(main())
